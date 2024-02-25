9154dfc88037ed181721a2565bb2281db084aad5
MissStefie <mariah.stefanie.delga@gmail.com>
2024-02-25 20:31:25 -0300

    Se crearon los archivos de Migracion del ER

--- database/migrations/2024_02_25_230137_crear_tabla_permiso.php
+++ database/migrations/2024_02_25_230137_crear_tabla_permiso.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('permiso', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 50);
+            $table->string('slog', 50);
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permiso');
     }
 };
