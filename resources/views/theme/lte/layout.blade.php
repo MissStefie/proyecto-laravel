e2950868b83c630e4347f2592e87d19a95ff48ae
MissStefie <mariah.stefanie.delga@gmail.com>
2024-03-07 16:45:31 -0300

    Tabla Roles

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset("assets/$theme/plugins/fontawesome-free/css/all.min.css") }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset("assets/$theme/dist/css/adminlte.min.css") }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     @yield('styles')
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 
@@ -41,6 +42,9 @@
     @yield('scriptsPlugins')
     <script src="{{ asset('assets/js/jquery-validation/jquery.validate.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery-validation/localization/messages_es.min.js') }}"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="{{ asset('assets/js/scripts.js') }}"></script>
     <script src="{{ asset('assets/js/funciones.js') }}"></script>
     @yield('scripts')
 </body>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use App\Http\Controllers\Admin\MenuController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\Admin\PermisoController;
+use App\Http\Controllers\Admin\RolController;
 
 Route::get('/', [InicioController::class, 'index']);
-//
 Route::get('admin/permiso', [PermisoController::class, 'index'])->name('permiso');
 Route::get('admin/permiso/crear', [PermisoController::class, 'crear'])->name('crear_permiso');
-//
 Route::get('admin/menu', [MenuController::class, 'index'])->name('menu');
 Route::get('admin/menu/crear', [MenuController::class, 'crear'])->name('crear_menu');
 Route::post('admin/menu', [MenuController::class, 'guardar'])->name('guardar_menu');
-Route::post('admin/menu/guardar-orden', 'MenuController@guardarOrden')->name('guardar_orden');
+Route::post('admin/menu/guardar-orden', [MenuController::class, 'guardarOrden'])->name('guardar_orden');
+Route::get('admin/rol', [RolController::class, 'index'])->name('rol');
+Route::get('admin/rol/crear', [RolController::class, 'crear'])->name('crear_rol');
+Route::post('admin/rol', [RolController::class, 'guardar'])->name('guardar_rol');
+Route::get('admin/rol/{id}/editar', [RolController::class, 'editar'])->name('editar_rol');
+Route::put('admin/rol/{id}', [RolController::class, 'actualizar'])->name('actualizar_rol');
+Route::delete('admin/rol/{id}', [RolController::class, 'eliminar'])->name('eliminar_rol');
